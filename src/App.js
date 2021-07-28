9b77099cea328b40c39c7ecfc71c42f6cf913d4a
rigole <foplacide@gmail.com>
2021-07-28 06:30:02 +0100

    creating homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import './App.css';
 
+import HomePage from "./homepage.component";
 function App() {
   return (
     <div>
-      HELLO
+      <HomePage/>
     </div>
   );
 }
