70eef786c406344003374a868d06740c7c1667b7
rigole <foplacide@gmail.com>
2021-08-01 20:59:29 +0100

    update menu items with withRouter() and setting the hats page link

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React  from "react";
 import './menu-item.styles.scss';
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
 
-    <div className = { `${size} menu-item`}>
+    <div className = { `${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={
             {backgroundImage: `url(${imageUrl})`}
         }/>
@@ -13,4 +14,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
         </div>
     </div>
 );
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './homepage.styles.scss';
 import Directory from "../../components/directory/directory.component";
-const HomePage = () => (
+const HomePage = ({ history }) => (
     <div className='homepage'>
-       <Directory />
+       <Directory history={history}/>
     </div>
 )
 
