b52ef6018e04ef3bee9a8cd67a734086bbaa602f
rigole <foplacide@gmail.com>
2021-08-03 09:05:53 +0100

    the shop page + setting collection preview component 1

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'preview-collection.styles.scss';
+import {findByDisplayValue} from "@testing-library/react";
+
+const PreviewCollectionComponent = () => (
+    <div className='collection-preview'>
+        <h1>Title</h1>
+        <div className='preview'>
+
+        </div>
+    </div>
+)
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import SHOP_DATA from "./shop.data";
 
 class ShopPage extends React.Component {
     constructor() {
         super();
 
         this.state = {
-
+            collections: SHOP_DATA
         }
     }
-
+    render() {
+        return(
+            <div>SHOP PAGE</div>
+        )
+    }
 }
+export default ShopPage;
