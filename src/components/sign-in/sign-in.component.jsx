594f1ceaef9d2c2034aa829d25bce46f29fdc878
rigole <foplacide@gmail.com>
2021-08-06 13:03:41 +0100

    creating the Sign in form for test

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,49 @@
 import React from "react";
-import 'sign-in.styles.scss'
+import './sign-in.styles.scss'
 
 class SignInComponent extends React.Component{
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
     }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target
+        this.setState({[name]: value})
+    }
+    render() {
+        return(
+            <div>
+                <h2> I already have an account</h2>
+                <span> Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>Email</label>
+                    <input name="email" type="email" onChange={this.handleChange} value={this.state.email} required/>
+                    <label>Password</label>
+                    <input
+                        name='password'
+                        type='password'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required
+
+                    />
+                    <input type="submit" value='Submit Form'/>
+                </form>
+            </div>
+        )
+    }
+
+
 }
+export default SignInComponent
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import './sign-in-and-sign-up.styles.scss'
+import SignInComponent from "../../components/sign-in/sign-in.component";
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>SIGN IN</div>
+    <div className='sign-in-and-sign-up'>
+        <SignInComponent/>
+    </div>
 )
 
 export default SignInAndSignUpPage
