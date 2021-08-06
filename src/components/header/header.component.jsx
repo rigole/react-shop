27fb56ed935c1c3f6c548cca0a426d2525b5eeb6
rigole <foplacide@gmail.com>
2021-08-06 08:10:43 +0100

    rendering the header(logo)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import './header.styles.scss'
+import './header.styles.scss';
+ import { Link } from 'react-router-dom';
+ import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 const Header = () => (
-    <div>
-
+    <div className='header'>
+        <Link className='logo-container' to="/">
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOPCONTACT
+            </Link>
+        </div>
     </div>
 )
+export default Header
 
