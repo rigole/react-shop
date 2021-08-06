e23c06b3d945e0fd25c2d3510cc4e8df7d564cc4
rigole <foplacide@gmail.com>
2021-08-06 16:49:56 +0100

    creating our own input components

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './form-input.styles.scss'
+
+const FormInput = ({ handleChange, label, ...othersProps }) => (
+    <div className='group'>
+        <input className='form-input' onChange={handleChange} {...othersProps}/>
+        {label ? (
+            <label
+                className={`${
+                    othersProps.value.length ? 'shrink' : ''
+                } form-input-label` }>
+                {label}
+            </label>
+        ): null}
+    </div>
+)
+
