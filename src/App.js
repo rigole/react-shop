d3e744a7b8ad9363b6dedf52f79c338a6c03d0f6
rigole <foplacide@gmail.com>
2021-08-08 21:56:40 +0100

    handling the signed-in used and unSubscription

--- src/App.js
+++ src/App.js
@@ -5,19 +5,44 @@ import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
 
-function App() {
-  return (
-    <div>
-        <Header/>
-        <Switch>
-            <Route exact path='/' component={ HomePage }/>
-            <Route path ='/shop' component={ ShopPage }/>
-            <Route path='/signin' component={SignInAndSignUpPage} />
-        </Switch>
-    </div>
-  );
+class App extends React.Component{
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null
+
+    componentDidMount() {
+        this.auth.onAuthStateChanged(user => {
+            this.setState({ currentUser: user })
+        })
+    }
+
+     componentWillUnmount() {
+        this.unsubscribeFromAuth();
+     }
+
+    render(){
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path='/' component={ HomePage }/>
+                    <Route path ='/shop' component={ ShopPage }/>
+                    <Route path='/signin' component={SignInAndSignUpPage} />
+                </Switch>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
