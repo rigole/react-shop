0a9c2d8c79391965930655d87eaa5005e7d0d398
rigole <foplacide@gmail.com>
2021-08-10 15:03:45 +0100

    create user profile if there's not in data ase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 class App extends React.Component{
@@ -19,8 +19,8 @@ class App extends React.Component{
 
     unsubscribeFromAuth = null
     componentDidMount() {
-         this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user })
+         this.unsubscribeFromAuth =  auth.onAuthStateChanged(async user => {
+            createUserProfileDocument(user)
         })
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const firebaseConfig = {
         "1:831191601785:web:048c2a1071134a412ffaa8"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists){
+        const { displayName, email } =  userAuth;
+        const createdAt = new Date();
+        
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+            
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
