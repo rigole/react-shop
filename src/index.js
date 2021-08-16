b61ffdd4237dad69c59f945c3dd1c6fdf10d2988
rigole <foplacide@gmail.com>
2021-08-16 19:47:40 +0100

    creating our reducer function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
 
   document.getElementById('root')
 );
