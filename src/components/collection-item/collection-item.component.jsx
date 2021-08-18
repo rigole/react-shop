cf3784f4defb797ccf408c52a7b3f817750211fe
rigole <foplacide@gmail.com>
2021-08-18 21:13:20 +0100

    button add to cart and its style

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './collection-item.styles.scss';
+import CustomButtonComponent from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ id, name, price, imageUrl}) => (
     <div className='collection-item'>
@@ -13,6 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButtonComponent>ADD TO CART</CustomButtonComponent>
     </div>
 )
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './custom-button.styles.scss'
 
-const CustomButtonComponent = ({ children, isGoogleSignIn ,...otherProps }) => (
+const CustomButtonComponent = ({ children, isGoogleSignIn, inverted ,...otherProps }) => (
     <button
-        className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+        className={`${inverted ? 'inverted' : '' }${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
         {...otherProps}
     >
         {children}
