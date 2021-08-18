db3401d3989a1ceeac48913e1b9e97a0e23066b6
rigole <foplacide@gmail.com>
2021-08-18 14:55:24 +0100

    adding the hidden feature on the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartDropdown   from "../cart-dropdown/cart-dropdown.component";
 import { connect } from "react-redux";
 
 
-const Header = ( {currentUser} ) => (
+const Header = ( {currentUser, hidden} ) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -39,12 +39,15 @@ const Header = ( {currentUser} ) => (
 
             <CartIconComponent/>
         </div>
-        <CartDropdown/>
+
+        { hidden ? null :  <CartDropdown/>}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
+
 export default connect(mapStateToProps)(Header)
 
