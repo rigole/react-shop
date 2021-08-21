44f31e7d44e4184e6598c4a3aa0c3463d58f5ee3
rigole <foplacide@gmail.com>
2021-08-21 18:16:07 +0100

    message in case the cart is empty

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,9 +10,16 @@ import { createStructuredSelector } from "reselect";
 const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
       <div className='cart-items'>
-          {cartItems.map(cartItem => (
-              <CartItem key={cartItem.id} item={cartItem}/>
-          ))}
+          {
+              cartItems.length ?
+                  (
+                         cartItems.map(cartItem => (
+                         <CartItem key={cartItem.id} item={cartItem}/>
+                 ))
+              ) : ( <span className='empty-message'>Your Cart is empty</span>)
+
+
+          }
       </div>
     <CustomButtonComponent>GO TO CHECKOUT</CustomButtonComponent>
   </div>
