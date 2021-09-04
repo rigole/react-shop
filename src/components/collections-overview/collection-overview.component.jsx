0b4f62850350ffdde578460e718a1a7ed7641b10
rigole <foplacide@gmail.com>
2021-09-04 15:10:06 +0100

    creating and setting the selectCollectionsForPreview in shop selector and in collection-overview component

--- src/components/collections-overview/collection-overview.component.jsx
+++ src/components/collections-overview/collection-overview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import './collection-overview.styles.scss'
 import PreviewCollectionComponent from "../preview-collection/preview-collection.component";
-import {selectCollections} from "../../redux/shop/shop.selectors";
+import {selectCollectionsForPreview} from "../../redux/shop/shop.selectors";
 
 const CollectionOverviewComponent = ({ collections }) => (
     <div className='collection-overview'>
@@ -14,7 +14,7 @@ const CollectionOverviewComponent = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverviewComponent)
